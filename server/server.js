6bba9e686979eaba95c7da638b0a7f22a600a338
Laurent Couvidou <lorancou@free.fr>
2014-04-27 16:41:54 +0200

    That's a long way to success

--- server/server.js
+++ server/server.js
@@ -23,15 +23,26 @@ io.sockets.on("connection", function(socket) {
         
         if (sanitizedMessage != "") {
             if (sanitizedMessage == "forward") {
-                duckPos += 0.1;
+                duckPos += 0.05;
             } else if (sanitizedMessage == "backward") {
-                duckPos -= 0.1;
+                duckPos -= 0.05;
             }
-            
+
             var fullMessage = { u: sanitizedUser, m: sanitizedMessage, d: duckPos };
-        
             io.sockets.emit("s",fullMessage);
             allMessages.push(fullMessage);
+
+            if (duckPos < 0.0) {
+                duckPos = 0.0;
+                var loseMessage = {u: "server", m: "Not this way", d: duckPos};
+                io.sockets.emit("s",loseMessage);
+                allMessages.push(loseMessage);
+            } else if (duckPos >= 1.0) {
+                duckPos = 0.0;
+                var winMessage = {u: "server", m: "Great success! {:V", d: duckPos};
+                io.sockets.emit("s",winMessage);
+                allMessages.push(winMessage);
+            }
         }
     });
 });
