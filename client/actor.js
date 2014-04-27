a6daa89a88e71446297a569259813fa21e892391
Laurent Couvidou <lorancou@free.fr>
2014-04-27 20:17:36 +0200

    Duck legs anim

--- client/actor.js
+++ client/actor.js
@@ -6,9 +6,27 @@ var gamejs = require('gamejs'),
 // Duck ctor
 exports.duck = function(scene, serverInfo) {    
     exports.duck.superConstructor.apply(this, arguments);
+    
     this.scene = scene;
-    this.originalImage = gamejs.image.load("./res/duck.png");
-    this.image = this.originalImage;
+
+    this.animFrame = 0;
+    this.animTime = 0;
+    this.ANIM_DELTA = 100;
+
+    this.originalImage = [
+        gamejs.image.load("./res/duck0.png"),
+        gamejs.image.load("./res/duck1.png"),
+        gamejs.image.load("./res/duck2.png"),
+        gamejs.image.load("./res/duck3.png"),
+        gamejs.image.load("./res/duck4.png"),
+        gamejs.image.load("./res/duck5.png"),
+        gamejs.image.load("./res/duck6.png"),
+    ];
+
+    this.image = this.originalImage[this.animFrame];
+
+    //this.originalImage = gamejs.image.load("./res/duck.png");
+    // this.image = this.originalImage;
     this.rect = this.getRect(serverInfo);
     this.floatBob = new utils.floatBob(0.003, 12);
     return this;
@@ -26,7 +44,16 @@ exports.duck.prototype.getRect = function(serverInfo) {
 
 // Duck update
 exports.duck.prototype.update = function(dt, serverInfo) {
+
+    // 7 frames anim
+    this.animTime += dt;
+    if (this.animTime > this.ANIM_DELTA)
+    {
+        this.animTime -= this.ANIM_DELTA;
+        this.animFrame = (this.animFrame + 1) % 7;
+    }
+
     this.rect = this.getRect(serverInfo);
     this.floatBob.update(dt);
-    this.image = gamejs.transform.rotate(this.originalImage, this.floatBob.rotation);
+    this.image = gamejs.transform.rotate(this.originalImage[this.animFrame], this.floatBob.rotation);
 }
